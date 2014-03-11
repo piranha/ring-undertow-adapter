6991df35cdeb395b423ed24ae9f9abf9fbe2e9fa
Alexander Solovyov <alexander@solovyov.net>
2014-03-11 18:02:31 +0200

    lets depend on newer version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject ring-undertow-adapter "0.1.3"
+(defproject ring-undertow-adapter "0.1.4"
   :description "Ring Underow adapter."
   :url "http://github.com/piranha/ring-adapter-undertow"
   :license {:name "ISC License"
             :url "http://opensource.org/licenses/ISC"}
-  :dependencies [[io.undertow/undertow-core "1.0.0.Beta8"]]
+  :dependencies [[io.undertow/undertow-core "1.0.0.Beta12"]]
   :profiles {:dev {:dependencies [[clj-http "0.7.6"]]}})
