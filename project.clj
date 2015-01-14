6e63876a4218e088c2fbe478b1b013f4fe7c7792
David Sargeant <dsargeant@roomkey.com>
2015-01-14 14:49:32 -0500

    Upped undertow version to 1.1.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/piranha/ring-adapter-undertow"
   :license {:name "ISC License"
             :url "http://opensource.org/licenses/ISC"}
-  :dependencies [[io.undertow/undertow-core "1.0.15.Final"]]
+  :dependencies [[io.undertow/undertow-core "1.1.0.Final"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [clj-http "1.0.1"]]}})
